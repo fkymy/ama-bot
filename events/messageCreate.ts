97c6f7e1ceb6598356679d2abd7e807dcea22078
fkymy <yuskefuku@gmail.com>
2022-03-03 11:42:43 +0900

    prototype

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -5,25 +5,37 @@ export default {
   once: false,
   execute(message: Message) {
     if (message.channel.type !== 'DM') return;
+    if (message.type === 'REPLY' && message.author.username === '質問箱bot') return;
 
-    // 1. get first word as channelID
-    const delim = message.content.indexOf(' ');
-    console.log('index', delim);
-    const channelId = message.content.substring(0, delim);
-    const reply = message.content.substring(delim + 1, message.content.length);
-    console.log(`channelId: '${channelId}'`);
-    console.log(`reply: '${reply}'`);
+    // 1. Get CHANNEL_ID and message in correct format
+    let channelId: string;
+    let reply: string;
 
-    // 2. send text to channel
-    console.log(message);
-    console.log(message.client);
-    console.log(message.client.user);
+    const words = message.content.split(' ');
+    if (words.length < 3) {
+      message.reply('Format: send [CHANNEL_ID] message...')
+      return;
+    }
+    if (words[0] !== 'send') {
+      message.reply('Format: send [CHANNEL_ID] message...')
+      return;
+    }
 
+    channelId = words[1];
+    reply = words.slice(2, words.length).join(' ');
+    if (reply.length > 140) {
+      message.reply('Message must be less than 140 words');
+      return;
+    }
+
+    // 2. Send text to channel
     const channel = message.client.channels.cache.get(channelId);
     if (channel && channel.type === 'GUILD_TEXT') {
-      (channel as TextChannel).send(reply);
-      message.react('👍');
+      (channel as TextChannel).send(reply)
+        .then(() => message.react('👍'))
+        .catch((err) => message.reply(`Error sending message: ${err}`));
+    } else {
+      message.reply(`Could not find channel with CHANNEL_ID: ${channelId}`);
     }
-    // message.reply({ content: reply });
   }
 }
